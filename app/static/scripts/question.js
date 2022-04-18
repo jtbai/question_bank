11fa4b7600f1898b378ed2bc4e0824cfc1cac411
Jean-François Dessureault <jfdess@gmail.com>
2022-04-18 16:24:48 -0400

    cleanup HTML & ajout de CSS + bouton next

--- app/static/scripts/question.js
+++ app/static/scripts/question.js
@@ -1,9 +1,14 @@
 const revealBtn = document.getElementById('reveal-btn')
+const nextBtn = document.getElementById('next-btn')
 const answer = document.getElementById('answer')
+const nextContainer = document.getElementById('next')
 
 revealBtn.addEventListener('click', () => showAnswer(revealBtn, answer))
+nextBtn.addEventListener('click', () => { window.location.reload() })
 
 function showAnswer (revealBtn, answer) {
     revealBtn.style.display = 'none'
     answer.style.display = 'inline'
+    nextContainer.style.display = 'block'
 }
+
